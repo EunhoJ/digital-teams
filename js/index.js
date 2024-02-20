2c22055e45ff332c65eafd2382a846c8fc6c2bde
EunhoJ <john.vieira99@gmail.com>
2024-02-20 12:04:08 -0300

    Criando multiplos cards

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let teams = []
+
 criarBtn.onclick = () => {
   overlay.classList.add("show");
   formCriar.classList.add("show");
@@ -13,23 +15,39 @@ overlay.onclick = () => {
   formCriar.classList.remove("show");
 };
 
+criarCard.onclick = () => {
+  if(nome.value !="" && capacidade.value != ""){
+    overlay.classList.remove("show");
+    formCriar.classList.remove("show");
+  }
+}
+
 formCriar.onsubmit = () => {
   event.preventDefault();
 
-  alert(nome.value + " " + capacidade.value);
+  teams.push({
+    name: nome.value,
+    capacity: capacidade.value,
+    members: []
+  })
+  viewCards();
+  overlay.classList.remove("show");
+  formCriar.classList.remove("show");
+};
+
+function viewCards(){
   listTeams.innerHTML = "";
-  listTeams.innerHTML = `
+  for(let i = 0; i <= teams.length; i++){
+  listTeams.innerHTML += `
       <li>
-          <h4>${nome.value}<box-icon name='show'></box-icon></h4>
-          <h1>0 <span>/ ${capacidade.value}</span></h1>
+          <h4>${teams[i].name}<box-icon name='show'></box-icon></h4>
+          <h1>0 <span>/ ${teams[i].capacity}</span></h1>
           <div class="actions">
               <button>adicionar</button>
               <button><box-icon id="deleteTeam" name='trash'></box-icon></button>
           </div>
       </li>
       
-      `;  
-  overlay.classList.remove("show");
-  formCriar.classList.remove("show");
-};
-
+      `;
+    }
+  }
