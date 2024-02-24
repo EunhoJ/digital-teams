c0a0c737b67474ed18ed4bd6da0160a3bbc19aa4
eu_nhoj <john.vieira99@gmail.com>
2024-02-24 18:05:18 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -1,53 +1,92 @@
-let teams = []
+let teams = [];
 
 criarBtn.onclick = () => {
-  overlay.classList.add("show");
-  formCriar.classList.add("show");
-};
+    overlay.classList.add('show');
+    formCriar.classList.add('show');
+}
 
 fecharBtn.onclick = () => {
-  overlay.classList.remove("show");
-  formCriar.classList.remove("show");
-};
+    overlay.classList.remove('show');
+    formCriar.classList.remove('show');
+}
+
+fecharParticipanteBtn.onclick = () => {
+    overlay.classList.remove('show');
+    formParticipante.classList.remove('show');
+}
 
 overlay.onclick = () => {
-  overlay.classList.remove("show");
-  formCriar.classList.remove("show");
-};
-
-criarCard.onclick = () => {
-  if(nome.value !="" && capacidade.value != ""){
-    overlay.classList.remove("show");
-    formCriar.classList.remove("show");
-  }
+    overlay.classList.remove('show');
+    formCriar.classList.remove('show');
+    formParticipante.classList.remove('show');
 }
 
 formCriar.onsubmit = () => {
-  event.preventDefault();
-
-  teams.push({
-    name: nome.value,
-    capacity: capacidade.value,
-    members: []
-  })
-  viewCards();
-  overlay.classList.remove("show");
-  formCriar.classList.remove("show");
-};
-
-function viewCards(){
-  listTeams.innerHTML = "";
-  for(let i = 0; i <= teams.length; i++){
-  listTeams.innerHTML += `
-      <li>
-          <h4>${teams[i].name}<box-icon name='show'></box-icon></h4>
-          <h1>0 <span>/ ${teams[i].capacity}</span></h1>
-          <div class="actions">
-              <button>adicionar</button>
-              <button><box-icon id="deleteTeam" name='trash'></box-icon></button>
-          </div>
-      </li>
-      
-      `;
+    event.preventDefault();
+    if(verificarLista(nome.value)){
+        alert("Este nome já está em uso");
+    }else{
+        teams.push({
+            name: nome.value,
+            capacity: capacidade.value,
+            members: []
+        });
+        adicionarCards();
+        overlay.classList.remove('show');
+        formCriar.classList.remove('show');
     }
-  }
+}
+
+formParticipante.onsubmit = () => {
+    event.preventDefault();
+    teams[Number(teamID.value)].members.push(nomeParticipante.value);
+    alert("Participante inserido com sucesso!");
+    formParticipante.reset();
+}
+
+function adicionarCards(){
+    listTeams.innerHTML = '';
+    if(teams.length === 0){
+        listTeams.innerHTML = '<li class="noTeams"><h4>Nenhum team criado ainda</h4></li>';
+        return;
+    }
+    for(let i = 0; i < teams.length; i++){
+        listTeams.innerHTML += `
+            <li>
+                <h4>${teams[i].name} <box-icon name='show'></box-icon></h4>
+                <h1>0 <span>/ ${teams[i].capacity}</span></h1>
+                <div class="actions">
+                    <button onClick="mostrarFormParticipante(${i})">adicionar</button>
+                    <button onClick="removerCard(${i})"><box-icon name='trash'></box-icon></button>
+                </div>
+            </li>
+        `;
+    }
+}
+
+function removerCard(indice){
+    let listaAuxiliar = [];
+    for(let i = 0; i < teams.length; i++){
+        if(i != indice){
+            listaAuxiliar.push(teams[i]);
+        }
+    }
+    teams = listaAuxiliar;
+    adicionarCards();
+}
+
+function verificarLista(nomeDoTeam){
+    let achou = false;
+    for(let i = 0; i < teams.length; i++){
+        if(teams[i].name === nomeDoTeam){
+            achou = true;
+        }
+    }
+    return achou;    
+}
+
+function mostrarFormParticipante(indice){
+    overlay.classList.add("show");
+    formParticipante.classList.add("show");
+    teamID.value = indice;
+}
